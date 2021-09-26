05d90c9a3bfb6b1f846edd29e2f5383c3528f296
apjammanbo <apjammanbo@gmail.com>
2021-09-27 00:55:53 +0900

    파비콘 및 로컬튜널 추가

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,18 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'ignore',
-  },
-  query: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'all',
-  },
-};
-
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://dotfeet.loca.lt/graphql',
   cache: new InMemoryCache({}),
   defaultOptions: {
     query: {
